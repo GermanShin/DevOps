30632c6a3982fc325b3a04ada28f81d1f6b0a6b5
David Shin <germanshin1217@gmail.com>
2026-03-30 14:21:45 +0800

    Deploy S3 bucket setup for report viewer infra stack

--- Infra/cdk/report-viewer-infra/lib/config.ts
+++ Infra/cdk/report-viewer-infra/lib/config.ts
@@ -1,9 +1,12 @@
 export interface Config {
   hostedZoneId: string;
   hostedZoneName: string;
+  reportBucketName?: string;
+  dummyIdAddress: string;
 }
 
 export const CONFIG: Config = {
   hostedZoneId: "Z0151506X0ZEQ6A3L0NT",
   hostedZoneName: "shared.ds-shin.com",
+  dummyIdAddress: "192.0.2.1",
 };
--- Infra/cdk/report-viewer-infra/lib/report-viewer-infra-stack.ts
+++ Infra/cdk/report-viewer-infra/lib/report-viewer-infra-stack.ts
@@ -9,6 +9,8 @@ export interface ReportViewerInfraStackProps extends cdk.StackProps {
 }
 
 export class ReportViewerInfraStack extends cdk.Stack {
+  public readonly slaReportBucket: s3.IBucket;
+
   constructor(
     scope: Construct,
     id: string,
@@ -25,6 +27,39 @@ export class ReportViewerInfraStack extends cdk.Stack {
       {
         hostedZoneId: cfg.hostedZoneId,
         zoneName: cfg.hostedZoneName,
+      }
+    );
+
+    // S3 Bucket for  Reports
+    // If you already have a bucket, use fromBucketName instead
+    const slaReportBucket = cfg.reportBucketName
+      ? s3.Bucket.fromBucketName(this, "SLAReportBucket", cfg.reportBucketName)
+      : new s3.Bucket(this, "SLAReportBucket", {
+          bucketName: `sla-reports-${this.account}`,
+          versioned: false,
+          encryption: s3.BucketEncryption.S3_MANAGED,
+          blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+          removalPolicy: cdk.RemovalPolicy.RETAIN, // Don't delete reports on stack deletion
+          autoDeleteObjects: false,
+          lifecycleRules: [
+            {
+              // Optional: Auto-delete old reports after 90 days
+              expiration: cdk.Duration.days(90),
+              enabled: false, // Set to true if you want auto-deletion
+            },
+          ],
+        });
+
+    this.slaReportBucket = slaReportBucket;
+
+    // Create Dummy A record for the parent domain for cognito custom domain
+    const parentDomainRecord = new route53.ARecord(
+      this,
+      "SlaReportCognitoDummyAliasRecord",
+      {
+        zone: zone,
+        target: route53.RecordTarget.fromIpAddresses(cfg.dummyIdAddress), //TEST-NET Dummy IP
+        recordName: "", //This automatically will use hosted zone apex (shared.ds-shin.com)
       }
     );
   }
